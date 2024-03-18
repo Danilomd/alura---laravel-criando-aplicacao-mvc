a40d3f3773a35514a05632fc9f31f1c3adb36b4a
Danilomd <danilomdaim@gmail.com>
2024-03-17 21:09:29 -0300

    Conclusão do curso

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -6,11 +6,13 @@
           content="width=device-width, user-scalable=no ,initial-sacale=1.0,maximum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>{{ $title }} - Controle de Séries</title>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>
 <body>
-<h1>{{ $title }}</h1>
+<div class="container">
+    <h1>{{ $title }}</h1>
 
-{{ $slot }}
-
+    {{ $slot }}
+</div>
 </body>
 </html>
--- webpack.mix.cjs
+++ webpack.mix.cjs
@@ -2,5 +2,4 @@ const mix = require('laravel-mix');
 
 
 mix
-    .sass('resources/css/app.css','public/css')
-    .js('resources/js/app.js','public/js')
+    .sass('resources/css/app.scss','public/css');
